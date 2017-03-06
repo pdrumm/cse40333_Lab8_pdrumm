250868a14857c92b3cf9a9a496d52c4d521cf008
pdrumm <pdrumm@nd.edu>
2017-03-05 19:33:55 -0500

    lab 5

--- app/src/main/java/com/cse40333/pdrumm/lab2_pdrumm/DetailActivity.java
+++ app/src/main/java/com/cse40333/pdrumm/lab2_pdrumm/DetailActivity.java
@@ -2,20 +2,36 @@ package com.cse40333.pdrumm.lab2_pdrumm;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.io.File;
+import android.provider.MediaStore;
+
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
+import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+import static java.security.AccessController.getContext;
 
 
 public class DetailActivity extends AppCompatActivity {
     private static final int CAMERA_INTENT = 1;
+    private File OUTPUT;
+    private String AUTHORITY = "com.cse40333.pdrumm.fileprovider";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +67,9 @@ public class DetailActivity extends AppCompatActivity {
 
         // Get images
         String mDrawableName = awayTeam.getTeamLogo();
-        int awayLogoId = getApplicationContext().getResources().getIdentifier(mDrawableName , "drawable", getPackageName());
+        int awayLogoId = getApplicationContext().getResources().getIdentifier(mDrawableName, "drawable", getPackageName());
         mDrawableName = homeTeam.getTeamLogo();
-        int homeLogoId = getApplicationContext().getResources().getIdentifier(mDrawableName , "drawable", getPackageName());
+        int homeLogoId = getApplicationContext().getResources().getIdentifier(mDrawableName, "drawable", getPackageName());
 
         // Format date
         DateFormat df = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH);
@@ -84,11 +100,44 @@ public class DetailActivity extends AppCompatActivity {
         View.OnClickListener camClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String filename = getPictureName();
+                File imagePath = new File(getApplicationContext().getFilesDir(), "images");
+                if (!imagePath.exists()) imagePath.mkdirs();
+                OUTPUT = new File(imagePath, filename);
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+                final Uri outputUri = FileProvider.getUriForFile(getApplicationContext(), AUTHORITY, OUTPUT);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+                getApplicationContext().grantUriPermission(
+                        "com.google.android.GoogleCamera",
+                        outputUri,
+                        FLAG_GRANT_WRITE_URI_PERMISSION | FLAG_GRANT_READ_URI_PERMISSION
+                );
+                cameraIntent.setFlags(FLAG_GRANT_WRITE_URI_PERMISSION);
                 startActivityForResult(cameraIntent, CAMERA_INTENT);
             }
         };
         cameraButton.setOnClickListener(camClick);
+    }
+
+    private String getPictureName() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String timestamp = sdf.format(new Date());
+        return "BestMoments" + timestamp + ".jpg";
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_INTENT) {
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                final Uri outputUri = FileProvider.getUriForFile(this, AUTHORITY, OUTPUT);
+                ImageView imgView = (ImageView) findViewById(R.id.detailImg);
+                imgView.setImageURI(outputUri);
+            }
+        }
     }
 
 }
